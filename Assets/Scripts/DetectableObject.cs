7914d6a199114b5755543ba35c18088b0eaacddc
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 00:08:00 +1100

    Added a thing that can tell if it is visible in the scene

---
+++ Assets/Scripts/DetectableObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectableObject : MonoBehaviour {
+
+	// This will be placed in a scene-wide registry of detectable objects...
+
+	public string metadata;
+	public Renderer myRender;
+
+	void Start() {
+		if(myRender == null) {
+			myRender = renderer;
+		}
+		if(myRender == null) {
+			enabled = false;
+		}
+	}
+
+	public bool CheckVisibility(Transform lookingFrom) {
+		if(!enabled) {
+			return false;
+		}
+		if(!myRender.isVisible) {
+			myRender.material.color = Color.red;
+			return false;
+		}
+		if(Physics.Linecast(transform.position, lookingFrom.position)) {
+			Debug.DrawLine(transform.position, lookingFrom.position, Color.red);
+			myRender.material.color = Color.yellow;
+			return false;
+		}
+		myRender.material.color = Color.blue;
+		return true;
+	}
+}
---
+++ Assets/Scripts/DetectableTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectableTest : MonoBehaviour {
+
+	public DetectableObject testing;
+
+	void Update() {
+		if(testing == null) {
+			return;
+		}
+		testing.CheckVisibility(transform);
+	}
+}
