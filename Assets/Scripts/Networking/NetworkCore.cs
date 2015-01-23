b1647fb29f87396df3f6f2d29d449a956ad6c94c
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 10:32:25 +1100

    Added a simple animation, added dummy functions for network core

---
+++ Assets/Scripts/Networking/NetworkCore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkCore : MonoBehaviour {
+
+	static int PORTNUM = 25038;
+
+	public void StartServer() {
+
+	}
+
+	public void JoinServer(string ip) {
+
+	}
+
+	public void StartGame() {
+
+	}
+
+	public bool IsConnected() {
+		return Network.peerType != NetworkPeerType.Disconnected;
+	}
+	public int GetConnections() {
+		return Network.connections.Length;
+	}
+}
