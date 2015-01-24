90aaad3ddae3790dc383afcc9fe4f805d93597e3
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 16:41:09 +1100

    Added classes for screenshot stuff

---
+++ Assets/Scripts/Animation/ScreenshotWithMetadata.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneralMetadata {
+	public string title;
+	public string body;
+	public Texture icon;
+}
+
+public class ScreenshotWithMetadata {
+	public RenderTexture screenshot;
+	Vector2[] positions;
+	GeneralMetadata[] data;
+}
