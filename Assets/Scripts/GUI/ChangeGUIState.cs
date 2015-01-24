fafedf92af7e3a92251ceb8090552cf6c2033bf5
hba <hba1990@gmail.com>
2015-01-24 11:23:03 +1100

    Created simple class which changes visibility of an object and enables another item

---
+++ Assets/Scripts/GUI/ChangeGUIState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeGUIState : MonoBehaviour 
+{
+    public GameObject otherDialog; // assign in the editor
+    public GameObject thisDialog;
+    private bool isSetup = false;
+
+    // Use this for initialization
+    void Start () 
+    {
+        if(otherDialog != null && thisDialog != null)
+        {
+            isSetup = true;
+        }
+    }
+    
+    public void ToggleDialog()
+    {
+        if(isSetup)
+        {
+            thisDialog.SetActive(false);
+            otherDialog.SetActive(true);
+        }
+    }
+}
