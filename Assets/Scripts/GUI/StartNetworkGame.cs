b2fb0f10cda43c6c548e25d90e5a06b623446c68
hba <hba1990@gmail.com>
2015-01-24 12:24:01 +1100

    Create start button check to see if all players are connected

---
+++ Assets/Scripts/GUI/StartNetworkGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StartNetworkGame : MonoBehaviour 
+{
+    [SerializeField] private Button startButton = null; // assign in the editor
+    private NetworkCore networkCore;
+
+    // Use this for initialization
+    void Start () 
+    {
+        startButton = GetComponent<Button>();
+        networkCore = GetComponent<NetworkCore>();
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        startButton.interactable = (networkCore.GetConnections() > 1);
+    }
+}
