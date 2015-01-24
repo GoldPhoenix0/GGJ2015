2e1e198160efc5e8c444aa2e14f93dd9c9b6559d
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-25 01:10:47 +1100

    Made screenshot buttons invisible when not mouse-overed, laid groundwork for metadata lookup in screenshot detected objects

--- Assets/Scripts/GUI/ScreenshotViewer.cs
+++ Assets/Scripts/GUI/ScreenshotViewer.cs
@@ -10,7 +10,19 @@ public class ScreenshotViewer : MonoBehaviour {
 
 	ScreenshotWithMetadata data;
 	List<Button> curButtons = new List<Button>();
+	List<ButtonDataManager> managers = new List<ButtonDataManager>();
 
+	class ButtonDataManager {
+		Vector2 position;
+		string key;
+		public ButtonDataManager(Vector2 pos, string key) {
+			position = pos;
+			this.key = key;
+		}
+		public void DataPressed() {
+			Debug.Log(key);
+		}
+	}
 
 	public void SetData(ScreenshotWithMetadata data) {
 		foreach(Button butt in curButtons) {
@@ -29,6 +41,10 @@ public class ScreenshotViewer : MonoBehaviour {
 			RectTransform curTrans = curButt.GetComponent<RectTransform>();
 			curTrans.SetParent(image.GetComponent<RectTransform>(), false);
 			curTrans.anchoredPosition = Vector2.Scale(data.positions[i], image.GetComponent<RectTransform>().sizeDelta);
+			ButtonDataManager newDat = new ButtonDataManager(data.positions[i], data.data[i]);
+			managers.Add(newDat);
+			curButt.onClick.AddListener(newDat.DataPressed);
 		}
 	}
+
 }
