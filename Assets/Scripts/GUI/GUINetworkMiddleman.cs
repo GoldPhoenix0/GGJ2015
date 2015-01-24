a60046d2cac02bfdcde93cb845ecafe007b2f470
hba <hba1990@gmail.com>
2015-01-24 12:09:44 +1100

    Created the join button functionality for the join server button

---
+++ Assets/Scripts/GUI/GUINetworkMiddleman.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUINetworkMiddleman : MonoBehaviour 
+{
+    [SerializeField] private InputField ipInputField = null; // assign in the editor
+    private NetworkCore networkCore;
+
+    // Use this for initialization
+    void Start () 
+    {
+        networkCore = GetComponent<NetworkCore>();
+    }
+    
+    public void SubmitIPAddress()
+    {
+        string ipAddress = ipInputField.text;
+
+        // Error Checking can go here
+
+        Debug.Log("IP Address = " + ipAddress);
+
+        networkCore.JoinServer(ipAddress);
+    }
+}
