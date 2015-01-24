e163cc508b113e753ae6687ada23b2a56bf8015b
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 19:18:04 +1100

    Added buttons to screenshot (no functionality yet)

--- Assets/Scripts/Animation/ScreenshotWithMetadata.cs
+++ Assets/Scripts/Animation/ScreenshotWithMetadata.cs
@@ -10,23 +10,18 @@ public class GeneralMetadata {
 }
 
 public class ScreenshotWithMetadata {
-	public Texture2D screenshot;
-	Vector2[] positions;
-	string[] data;
+	public Texture screenshot;
+	public Vector2[] positions;
+	public string[] data;
 
 	public ScreenshotWithMetadata(Camera takeFrom, DetectableObjectRegistry reg) {
-		Texture2D tex = new Texture2D(takeFrom.targetTexture.width, takeFrom.targetTexture.height);
-		RenderTexture.active = takeFrom.targetTexture;
-		tex.ReadPixels(new Rect(0, 0, tex.width, tex.height), tex.width, tex.height);
-		tex.Apply();
-		screenshot = tex;
+		screenshot = takeFrom.targetTexture;
 		List<DetectableObject> objectsHere = reg.GetCurrentlyInView(takeFrom.transform);
 		positions = new Vector2[objectsHere.Count];
 		data = new string[objectsHere.Count];
 		for(int i = 0; i < objectsHere.Count; ++i) {
 			positions[i] = takeFrom.WorldToViewportPoint(objectsHere[i].transform.position);
 			data[i] = objectsHere[i].metadataKey;
-			Debug.Log(positions[i]);
 		}
 	}
 }
