35da3469ba8b9887cfe5e222c2ed930f86f7eaed
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 14:29:33 +1100

    Made main menu OVR compatible, made server start automatically on 'eyes'

---
+++ Assets/HackyNetworkStarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HackyNetworkStarter : MonoBehaviour {
+
+	[SerializeField] NetworkCore core;
+
+	void Start() {
+		if(Screen.currentResolution.width == 1280 && Screen.currentResolution.height == 800) {
+			core.StartServer();
+		}
+	}
+}
