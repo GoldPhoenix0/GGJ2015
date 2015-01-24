05b86e171cf33da4f16c93bc6cf5f28efa77b61d
hba <hba1990@gmail.com>
2015-01-24 19:22:09 +1100

    Created a onclick listener to be added dynamically to the buttons when created

---
+++ Assets/Scripts/GUI/DynamicButtonOnClickListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DynamicButtonOnClickListener : MonoBehaviour 
+{
+    private Button button;
+    private Text title;
+    private PopulateDetailedView detailedView = null;
+
+    // Use this for initialization
+    void Start () 
+    {
+        button = this.GetComponent<Button>();
+        title = this.GetComponentInChildren<Text>();
+
+        button.onClick.AddListener(() => { OnButtonClicked(); }); 
+    }
+
+    public void SetDetailedView(PopulateDetailedView detail)
+    {
+        detailedView = detail;
+    }
+    
+    public void OnButtonClicked()
+    {
+        detailedView.InitialiseDetailedView(title.text);
+    }
+}
