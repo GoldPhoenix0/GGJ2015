d39755285e6e1768e180948a190622e17dd8aaf6
Kieren Wallace <kierenwallace@senet.com.au>
2015-01-24 12:06:06 +1100

    Added groundwork for screenshot sending/receiving

---
+++ Assets/Scripts/Animation/LocalRotationQuery.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalRotationQuery : MonoBehaviour {
+
+	public Transform observed;
+
+	public Quaternion GetLocalRotation() {
+		return observed.localRotation;
+	}
+}
