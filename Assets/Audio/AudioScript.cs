c20bdcd200d2fa6133635a17dba2af243770c67a
Cherie <Cherie.Davidson@rmit.edu.au>
2015-01-25 17:10:39 +1100

    I don't know what I'm commiting but I think it's the latest database and audio shit script

---
+++ Assets/Audio/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioScript : MonoBehaviour {
+
+	[SerializeField] AudioClip[] clips;
+	AudioSource audio;
+
+	void Start(){
+		audio = GetComponent<AudioSource>();
+	}
+
+	public void PlayAudio (int audioIndex){
+		if (clips[audioIndex] != null){
+			audio.clip = clips[audioIndex];
+			audio.Play();
+		}
+	}
+
+}
